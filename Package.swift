d4dfe7d2f8bd9c74588a1bfbf0c9f290a21757e8
mark <markldn@gmail.com>
2025-06-23 21:18:18 +0200

    Restrict target to src directory and set publicHeadersPath for SwiftPM library compatibility

--- Package.swift
+++ Package.swift
@@ -11,7 +11,8 @@ let package = Package(
     targets: [
         .target(
             name: "llama",
-            path: "./",
+            path: "src",
+            publicHeadersPath: "../include",
             exclude: [
                 "tests", "examples", "docs", "media", "models", "vendor", "gguf-py", "requirements.txt", "pyproject.toml", "poetry.lock", "mypy.ini", "pyrightconfig.json", "CMakeLists.txt", "CMakePresets.json", "Makefile", "build-xcframework.sh", "convert_hf_to_gguf.py", "convert_hf_to_gguf_update.py", "convert_llama_ggml_to_gguf.py", "convert_lora_to_gguf.py", "flake.lock", "flake.nix", "CODEOWNERS", "CONTRIBUTING.md", "SECURITY.md", "LICENSE", "AUTHORS", "licenses",
                 "tools/server/webui/src/main.tsx", "tools/main/main.cpp", "examples/batched.swift/Sources/main.swift"
