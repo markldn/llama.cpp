6b0de6b89079570045f870bb9d78de15ffa95426
mark <markldn@gmail.com>
2025-06-23 16:41:32 +0200

    Add minimal SwiftPM manifest for Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "llama.cpp",
+    products: [
+        .library(
+            name: "llama",
+            targets: ["llama"]),
+    ],
+    targets: [
+        .target(
+            name: "llama",
+            path: "./",
+            exclude: [
+                "tests", "examples", "docs", "media", "models", "vendor", "gguf-py", "requirements.txt", "pyproject.toml", "poetry.lock", "mypy.ini", "pyrightconfig.json", "CMakeLists.txt", "CMakePresets.json", "Makefile", "build-xcframework.sh", "convert_hf_to_gguf.py", "convert_hf_to_gguf_update.py", "convert_llama_ggml_to_gguf.py", "convert_lora_to_gguf.py", "flake.lock", "flake.nix", "CODEOWNERS", "CONTRIBUTING.md", "SECURITY.md", "LICENSE", "AUTHORS", "licenses"],
+            sources: ["src", "common", "include", "ggml"]
+        )
+    ]
+) 
