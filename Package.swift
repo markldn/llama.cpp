6eac4ea6ed00c62fcef3c14841a9cc909f5638ae
mark <markldn@gmail.com>
2025-06-23 16:46:52 +0200

    Exclude all main.* files from library target for SwiftPM compatibility

--- Package.swift
+++ Package.swift
@@ -13,7 +13,9 @@ let package = Package(
             name: "llama",
             path: "./",
             exclude: [
-                "tests", "examples", "docs", "media", "models", "vendor", "gguf-py", "requirements.txt", "pyproject.toml", "poetry.lock", "mypy.ini", "pyrightconfig.json", "CMakeLists.txt", "CMakePresets.json", "Makefile", "build-xcframework.sh", "convert_hf_to_gguf.py", "convert_hf_to_gguf_update.py", "convert_llama_ggml_to_gguf.py", "convert_lora_to_gguf.py", "flake.lock", "flake.nix", "CODEOWNERS", "CONTRIBUTING.md", "SECURITY.md", "LICENSE", "AUTHORS", "licenses"],
+                "tests", "examples", "docs", "media", "models", "vendor", "gguf-py", "requirements.txt", "pyproject.toml", "poetry.lock", "mypy.ini", "pyrightconfig.json", "CMakeLists.txt", "CMakePresets.json", "Makefile", "build-xcframework.sh", "convert_hf_to_gguf.py", "convert_hf_to_gguf_update.py", "convert_llama_ggml_to_gguf.py", "convert_lora_to_gguf.py", "flake.lock", "flake.nix", "CODEOWNERS", "CONTRIBUTING.md", "SECURITY.md", "LICENSE", "AUTHORS", "licenses",
+                "tools/server/webui/src/main.tsx", "tools/main/main.cpp", "examples/batched.swift/Sources/main.swift"
+            ],
             sources: ["src", "common", "include", "ggml"]
         )
     ]
